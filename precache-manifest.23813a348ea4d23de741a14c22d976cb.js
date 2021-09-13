ce4f569e02ab292441a17c6b0d1f1c6d051e9b3e
foul-soul <150rupyadega@gmail.com>
2021-09-13 18:40:50 +0530

    Updates

---
+++ precache-manifest.23813a348ea4d23de741a14c22d976cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cee43acf9d267772306bdc49a33d7d",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "f06dfaa2a9cc5b12b741",
+    "url": "/react1/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "722f5c68d774e8f3e398",
+    "url": "/react1/static/js/2.740e0d29.chunk.js"
+  },
+  {
+    "revision": "f06dfaa2a9cc5b12b741",
+    "url": "/react1/static/js/main.f7dff9c8.chunk.js"
+  },
+  {
+    "revision": "b90dec14f34c79c9a090",
+    "url": "/react1/static/js/runtime-main.5d38cc21.js"
+  }
+]);
